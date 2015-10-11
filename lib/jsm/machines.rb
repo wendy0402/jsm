c8fdf96f15833e93793f53e2514bd3b5a7c3cb19
wendy0402 <wendykurniawan92@gmail.com>
2015-10-11 20:33:05 +0700

    don't use cattr_reader(its from rails not from ruby)

--- lib/jsm/machines.rb
+++ lib/jsm/machines.rb
@@ -1,7 +1,10 @@
 class Jsm::Machines
-  cattr_reader :machines
-  @@machines = {}
   def self.add_machines(klass, state_machine)
+    @@machines ||= {}
     @@machines[klass.to_s] = state_machine
+  end
+
+  def self.machines
+    @@machines ||= {}
   end
 end
