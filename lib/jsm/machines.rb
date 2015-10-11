63ecf145e462f8e7e32a42d250ba37853a784be5
wendy0402 <wendykurniawan92@gmail.com>
2015-10-11 19:59:17 +0700

    use cattr_reader instead

--- lib/jsm/machines.rb
+++ lib/jsm/machines.rb
@@ -1,10 +1,7 @@
 class Jsm::Machines
+  cattr_reader :machines
   @@machines = {}
   def self.add_machines(klass, state_machine)
     @@machines[klass.to_s] = state_machine
-  end
-
-  def self.machines(klass)
-    @@machines[klass.to_s]
   end
 end
