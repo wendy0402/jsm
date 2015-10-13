e50f196771aef93fb86b2706bae7d7665411a5a9
wendy0402 <wendykurniawan92@gmail.com>
2015-10-13 21:27:24 +0700

    add explanation for client extension

--- lib/jsm/client_extension.rb
+++ lib/jsm/client_extension.rb
@@ -1,3 +1,5 @@
+# Jsm::ClientExtension define all custom methods for the model that use the state machine class
+# E.g: Define method for states
 class Jsm::ClientExtension
   def self.decorate(klass, params = {})
     client_extension = new(klass, params)
@@ -11,6 +13,9 @@ class Jsm::ClientExtension
     @state_machine = params[:state_machine]
   end
 
+  # define method for all states to check status equal with a states
+  # e.g: states: [:x, :y]
+  # it will define method `x?` and `y?`
   def define_states_method
     state_machine.states.each do |state|
       state_name = state.name
@@ -20,6 +25,11 @@ class Jsm::ClientExtension
     end
   end
 
+  # define all event that has been defined
+  # it consists of 3 types of method:
+  # * method to check whether can do event( e.g: can_married? )
+  # * method to run an event and return bollean( e.g: married )
+  # * method to run an event and raise error if failed( e.g: married! )
   def define_event_method
     state_machine.events.each do |event_name, event|
       define_can_event_method(event_name, event)
