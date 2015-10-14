b2f7647d8413dd3d4b7f78d7c62ae55de6ecfe77
wendy0402 <wendykurniawan92@gmail.com>
2015-10-14 15:43:03 +0700

    group by exception type

--- lib/jsm/exceptions.rb
+++ lib/jsm/exceptions.rb
@@ -1,6 +1,9 @@
 class Jsm::NotUniqueStateError < StandardError; end
 class Jsm::InvalidStateError < StandardError; end
+
 class Jsm::InvalidTransitionError < StandardError; end
-class Jsm::InvalidEventError < StandardError;end
-class Jsm::NoAttributeError < StandardError;end
 class Jsm::IllegalTransitionError < StandardError;end
+
+class Jsm::InvalidEventError < StandardError;end
+
+class Jsm::NoAttributeError < StandardError;end
