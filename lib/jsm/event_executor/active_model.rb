69b3733473b9d070456d2a7bc7c2ac695747934b
wendy0402 <wendykurniawan92@gmail.com>
2015-10-18 09:33:11 +0700

    delete unneeded code

--- lib/jsm/event_executor/active_model.rb
+++ lib/jsm/event_executor/active_model.rb
@@ -1,15 +1,4 @@
 class Jsm::EventExecutor::ActiveModel < Jsm::EventExecutor::Base
-  # it execute event for the object.
-  # If transition failed or invalid by validation toward the object,
-  # then it will return false
-  def execute(event, obj)
-    if can_be_executed?(event, obj)
-      event.execute(obj)
-    else
-      false
-    end
-  end
-
   # check if the obj possible to execute the event
   def can_be_executed?(event, obj)
     state = event.can_be_transitioning_to(obj)
