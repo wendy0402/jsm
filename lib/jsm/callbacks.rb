b811ea1f8f8b8a9c24d68baab389ddf0ef6c5674
wendy0402 <wendykurniawan92@gmail.com>
2015-10-29 07:41:30 +0700

    refactor callbacks

--- lib/jsm/callbacks.rb
+++ lib/jsm/callbacks.rb
@@ -1,18 +1,33 @@
 module ::Jsm::Callbacks
   def self.included(base)
-    base.instance_variable_set(:@jsm_callbacks,Jsm::Callbacks::ChainCollection.new(base))
-    base.class_eval("def self.jsm_callbacks; @jsm_callbacks;end", __FILE__, __LINE__)
     base.extend ClassMethods
     base.send(:include, InstanceMethods)
   end
 
   module ClassMethods
-    def before(context, &block)
-      callback = Jsm::Callbacks::Callback.new(:before, &block)
-      self.jsm_callbacks[context].insert_callback(callback)
+    def jsm_callbacks
+      @jsm_callbacks ||= Jsm::Callbacks::ChainCollection.new(self)
+    end
+    #override this to do something before method `before`
+    def pre_before(context, &block)
     end
 
+    #override this to do something before method `after`
+    def pre_after(context, &block)
+    end
+
+    # this method is to register a `before callback` to a jsm_callbacks
+    def before(context, &block)
+
+      pre_before(context, &block)
+      callback = Jsm::Callbacks::Callback.new(:before, &block)
+      jsm_callbacks[context] ||= Jsm::Callbacks::Chain.new(context)
+      jsm_callbacks[context].insert_callback(callback)
+    end
+
+    # this method is to register a `after callback` to a jsm_callbacks
     def after(context, &block)
+      pre_after(context, &block)
       callback = Jsm::Callbacks::Callback.new(:after, &block)
       self.jsm_callbacks[context].insert_callback(callback)
     end
